a488085d0956d228c2486387c95607e37015587a
GBRJo <anchernyakova@yandex.ru>
2023-10-14 07:10:31 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,47 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    const zeroAndOne = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        zeroAndOne.push(expr.substring(i, i + 10));
+    }
+    
+    const dotAndSlash = [];
+    for (let i = 0; i < zeroAndOne.length; i++) {
+        dotAndSlash.push(zeroAndOne[i].replace(/10/g, '.').replace(/11/g, '-'));
+    }
+
+    const dotAndSlashPure = [];
+    for (let i = 0; i < dotAndSlash.length; i++) {
+        dotAndSlashPure.push(dotAndSlash[i].replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').replace(/00/g, ''));
+    }
+
+    for (let i = 0; i < dotAndSlashPure.length; i++) {
+        const key = dotAndSlashPure[i];
+        if (MORSE_TABLE.hasOwnProperty(key)) {
+            dotAndSlashPure[i] = MORSE_TABLE[key];
+    }
 }
 
-module.exports = {
-    decode
-}
+
+    const dotAndSlashString = dotAndSlashPure.join('');
+
+   
+
+    
+   
+    return dotAndSlashString
+
+    }
+
+    module.exports = {
+        decode
+      };
+   
+    
+
+  
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
